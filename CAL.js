d52633701a298362c1318a27fd51c04e24c4a7ca
Дима <Rockes350@gmail.com>
2018-04-05 16:55:19 +0300

    js2

--- CAL.js
+++ CAL.js
@@ -1,4 +1,19 @@
-var buttonPlus = document.getElementById ("buttonPlus")
-var buttonMinus = document.getElementById ("buttonMinus")
-var buttonMultiphy = document.getElementById ("buttonMultiphy")
-var buttonDivide = document.getElementById ("buttonDevide")
+var buttonPlus = document.getElementById ("buttonPlus");
+var buttonMinus = document.getElementById ("buttonMinus");
+var buttonMultiphy = document.getElementById ("buttonMultiphy");
+var buttonDivide = document.getElementById ("buttonDevide");
+
+function onButtonPlusClick() {
+    console.log("onButtonPlusClick")
+}
+function onButtonMinusClick() {
+    console.log("onButtonMinusClick")
+}
+function onButtonMultiphyClick() {
+    console.log("onButtonMultiphyClick")
+}
+function onButtonDivideClick() {
+    console.log("onButtonDivideClick")
+}
+
+buttonPlus.addEventListener("click", onButtonPlusClick)
